e264a13d687d31f31151ee38c43eb5f62e4a2745
bemolina91 <bemolina9@icloud.com>
2023-03-01 11:58:45 -0600

    added myFetch fxn info

--- scriptHelper.js
+++ scriptHelper.js
@@ -66,7 +66,8 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then(function (response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        return response.json();
     });
 
     return planetsReturned;
