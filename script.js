710f8f635caec11d945e8723c889b7013f4e6e45
bemolina91 <bemolina9@icloud.com>
2023-03-01 12:55:20 -0600

    finished calling a chosen planet

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function () {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
-        console.log(listedPlanets);
+        console.log(listedPlanets);        
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let chosenPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diameter, chosenPlanet.star, chosenPlanet.distance, chosenPlanet.moons, chosenPlanet.imageUrl);
     })
 
     let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
     let form = document.querySelector("form");
 
     form.addEventListener("submit", function (event) {
